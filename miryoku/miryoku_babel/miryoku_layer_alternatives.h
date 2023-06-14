c7209c4bee79156940790d8838ed581ee7e9b5b7
antlers <antlers@illucid.net>
2023-06-14 19:18:23 +0000

    add magic_sturdy layout

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -66,6 +66,12 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
 
+#define MIRYOKU_ALTERNATIVES_BASE_MAGIC_STURDY_FLIP \
+&kp V,             &kp M,             &kp L,             &kp C,             &kp P,             &kp B,             &key_repeat,       &kp U,             &kp O,             &kp Q,           \
+U_MT(LGUI, S),     U_MT(LALT, T),     U_MT(LCTRL, R),    U_MT(LSHFT, D),    &kp Y,             &kp F,             U_MT(LSHFT, R),    U_MT(LCTRL, E),    U_MT(LALT, A),     U_MT(LGUI, I),     \
+U_LT(U_BUTTON, X), U_MT(RALT, K),     &kp J,             &kp G,             &kp W,             &kp Z,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SEMI),\
+U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
+
 #define MIRYOKU_ALTERNATIVES_BASE_AZERTY \
 &kp A,             &kp Z,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, Q),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, M),     \
@@ -124,6 +130,12 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_BASE_MAGIC_STURDY \
+&kp V,             &kp M,             &kp L,             &kp C,             &kp P,             &kp B,             &key_repeat,       &kp U,             &kp O,             &kp Q,           \
+U_MT(LGUI, S),     U_MT(LALT, T),     U_MT(LCTRL, R),    U_MT(LSHFT, D),    &kp Y,             &kp F,             U_MT(LSHFT, R),    U_MT(LCTRL, E),    U_MT(LALT, A),     U_MT(LGUI, I),     \
+U_LT(U_BUTTON, X), U_MT(RALT, K),     &kp J,             &kp G,             &kp W,             &kp Z,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SEMI),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 
@@ -187,6 +199,12 @@ U_NP,              U_NP,              &kp DEL,           &kp BSPC,          &kp 
 &kp Y,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
 U_NP,              U_NP,              &kp DEL,           &kp BSPC,          &kp RET,           &kp TAB,           &kp SPACE,         &kp ESC,           U_NP,              U_NP
 
+#define MIRYOKU_ALTERNATIVES_TAP_MAGIC_STURDY_FLIP \
+&kp V,             &kp M,             &kp L,             &kp C,             &kp P,             &kp B,             &key_repeat,       &kp U,             &kp O              &kp Q, \
+&kp S,             &kp T,             &kp R,             &kp D,             &kp Y,             &kp F,             &kp N,             &kp E,             &kp A              &kp I, \
+&kp X,             &kp K,             &kp J,             &kp G,             &kp W,             &kp Z,             &kp H,             &kp COMMA,         &kp DOT,           &kp SEMI, \
+U_NP,              U_NP,              &kp DEL,           &kp BSPC,          &kp RET,           &kp TAB,           &kp SPACE,         &kp ESC,           U_NP,              U_NP
+
 #define MIRYOKU_ALTERNATIVES_TAP_AZERTY \
 &kp A,             &kp Z,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp Q,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp M,             \
@@ -245,6 +263,12 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Y,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_TAP_MAGIC_STURDY \
+&kp V,             &kp M,             &kp L,             &kp C,             &kp P,             &kp B,             &key_repeat,       &kp U,             &kp O              &kp Q, \
+&kp S,             &kp T,             &kp R,             &kp D,             &kp Y,             &kp F,             &kp N,             &kp E,             &kp A              &kp I, \
+&kp X,             &kp K,             &kp J,             &kp G,             &kp W,             &kp Z,             &kp H,             &kp COMMA,         &kp DOT,           &kp SEMI, \
 U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 
