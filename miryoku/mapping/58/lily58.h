0a183c241f5f37aa6a7674b872a768b23a38d0a7
antlers <antlers@illucid.net>
2023-06-30 07:23:23 +0000

    or this

--- miryoku/mapping/58/lily58.h
+++ miryoku/mapping/58/lily58.h
@@ -1,30 +1,6 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#include <behaviors.dtsi>
-
-/ {
-  macros {
-      dbl_l: dbl_l {
-        compatible = "zmk,behavior-macro";
-        label = "ZM_dbl_l";
-        #binding-cells = <0>;
-        wait-ms = <30>;
-        tap-ms = <40>;
-        bindings = <&kp L &kp L>;
-      };
-  };
-  combos {
-      compatible = "zmk,combos";
-      combo_l_mr {
-          timeout-ms = <50>;
-          // layers = <0>;
-          key-positions = <22 23>;
-          bindings = <&dbl_l>;
-      };
-  };
-};
-
 #if !defined (MIRYOKU_LAYOUTMAPPING_LILY58)
 
 #define XXX &none
