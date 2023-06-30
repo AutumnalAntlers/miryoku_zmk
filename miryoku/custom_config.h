289c9806a07532fdd6ceddafc6bb38cc77298d5c
antlers <antlers@illucid.net>
2023-06-30 05:37:31 +0000

    test first combo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,22 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+/ {
+    macros {
+        ZMK_MACRO(dbl_l,
+            wait-ms = <30>;
+            tap-ms = <40>;
+            bindings = <&kp l &kp l>;
+        )
+        };
+    };
+    combos {
+        compatible = "zmk,combos";
+        combo_l_mr {
+            // timeout-ms = <50>;
+            layers = <0>;
+            key-positions = <22 23>;
+            bindings = <&dbl_l>;
+        };
+    };
+};
