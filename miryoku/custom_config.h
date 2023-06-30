9d726249bde01458690a0462fd6590a96a2ef630
antlers <antlers@illucid.net>
2023-06-30 05:46:39 +0000

    how bout now

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,21 +1,27 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-/ {
-    macros {
-        ZMK_MACRO(dbl_l,
-            wait-ms = <30>;
-            tap-ms = <40>;
-            bindings = <&kp l &kp l>;
-        )
-    };
-    combos {
-        compatible = "zmk,combos";
-        combo_l_mr {
-            // timeout-ms = <50>;
-            layers = <0>;
-            key-positions = <22 23>;
-            bindings = <&dbl_l>;
-        };
-    };
+#include <behaviors.dtsi>
+
+\ {
+  macros {
+      dbl_l: dbl_l {
+        compatible = "zmk,behavior-macro";
+        label = "ZM_dbl_l";
+        #binding-cells = <0>;
+        layers = <0>;
+        wait-ms = <30>;
+        tap-ms = <40>;
+        bindings = <&kp l &kp l>;
+      };
+  };
+  combos {
+      compatible = "zmk,combos";
+      combo_l_mr {
+          // timeout-ms = <50>;
+          layers = <0>;
+          key-positions = <22 23>;
+          bindings = <&dbl_l>;
+      };
+  };
 };
